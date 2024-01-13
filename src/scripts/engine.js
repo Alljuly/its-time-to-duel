1bde7b9818f991ed7edfa30f9335c0b83a7defde
alice <ajsc1@aluno.ifal.edu.brm>
2024-01-13 13:52:38 -0300

    feat: drawCards, cardData

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -23,8 +23,47 @@ const players = {
 	player2: "computer-cards",
 };
 
+const imgPath = ".src/assets/icons/";
+
+const cardData = [
+	{
+		id: 0,
+		name: "Blue Eyes White Dragon",
+		type: "Paper",
+		img: `${imgPath}dragon.png`,
+        winOf : [1],
+        loseOf: [2]
+	},
+    {
+        id: 1,
+        name: "Dark Magician",
+        type: "Rock",
+        img: `${pathImages}magician.png`,
+        winOf: [2],
+        LoseOf: [0]
+    },
+    {
+         id: 2,
+        name: "Exodia",
+        type: "Scissors",
+        img: `${pathImages}exodia.png`,
+        winOf: [0],
+        LoseOf: [2]
+    }
+];
 
 
-function init() {}
+async function drawCards(cardNumbers, fieldSide){
+    for(let i = 0; i < cardNumbers; i++){
+        const randomIdCard = await getRandomCardId()
+        const cardImage = await creatCardImage 
+    }
+
+} 
+
+function init() {
+    drawCards(5, "player")
+    drawCards(5, "computer")
+}
 
 init();
